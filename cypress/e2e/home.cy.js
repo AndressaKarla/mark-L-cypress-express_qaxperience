dd633f0773f9113c42bc2674937f6f67f92a3e34
Andressa Karla <andressa.karla1@gmail.com>
2023-04-26 09:59:08 -0300

    Atualizando novamente home.cy.js com um tempo de espera
    para verificar se a execução daria certo ou não no GitHub Actions

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,9 +3,9 @@
 describe('home', () => {
   it('webapp deve estar online', () => {
     cy.visit('/')
-
-    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
     
     cy.wait(5000)
+  
+    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
   })
 })
