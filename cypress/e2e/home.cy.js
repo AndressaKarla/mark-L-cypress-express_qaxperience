965326261c7c39e7765f489e5530e7ca35e6b3bc
Andressa Karla <andressa.karla1@gmail.com>
2023-04-26 10:05:19 -0300

    Removendo o tempo de espera de home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,8 +4,6 @@ describe('home', () => {
   it('webapp deve estar online', () => {
     cy.visit('/')
     
-    cy.wait(5000)
-  
     cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
   })
 })
