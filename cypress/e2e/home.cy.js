032d72f9ed5b7506b16e31c4ef5aac3f506d322d
AndressaKarla <andressa.karla1@gmail.com>
2023-07-05 18:56:28 -0300

    Alterando home.cy.js para ter Dado, Quando e Então sem Cucumber

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,23 @@
 /// <reference types="cypress" />
 
-describe('home', () => {
-  it('webapp deve estar online', () => {
-    cy.visit('/')
-    
-    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
+/* home.cy.js
+ *
+ * Feature: Home
+ *   Como um usuário do site Mark L
+ *   Quero executar alguns casos de testes 
+ *   Para validar a feature Home
+ */
+
+ describe('Home', () => {
+  context("Dado que eu esteja na página Home do site Mark L", () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    context("Quando eu verificar o título", () => {
+      it("Então deve ser apresentado o título Gerencie suas tarefas com Mark L", () => {
+        cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
+      })
+    })
   })
 })
